e9ea18570a0eebdd4df62c2f277a19bf4965ddbb
Stuart2307 <c00284845@setu.ie>
2024-06-27 16:39:16 +0100

    Added more operations, up to RRA

--- src/CPU.rs
+++ src/CPU.rs
@@ -138,7 +138,14 @@ enum Instructions
     AND(ArithmeticTarget),
     OR(ArithmeticTarget),
     XOR(ArithmeticTarget),
-    CP(ArithmeticTarget)
+    CP(ArithmeticTarget),
+    INC8(ArithmeticTarget),
+    INC16(ArithmeticTarget16),
+    DEC8(ArithmeticTarget),
+    DEC16(ArithmeticTarget16),
+    CCF(),
+    SCF(),
+    RRA()
 }
 enum ArithmeticTarget
 {
@@ -268,7 +275,65 @@ impl CPU
                         ArithmeticTarget::H => {self.cp(self.registers.h);}
                         ArithmeticTarget::L => {self.cp(self.registers.l);}
                     }
-                }  
+                }
+                Instructions::INC8(target) =>
+                {
+                    match target
+                    {
+                        ArithmeticTarget::A => {self.registers.a = self.inc_8(self.registers.a);}
+                        ArithmeticTarget::B => {self.registers.b = self.inc_8(self.registers.b);}
+                        ArithmeticTarget::C => {self.registers.c = self.inc_8(self.registers.c);}
+                        ArithmeticTarget::D => {self.registers.d = self.inc_8(self.registers.d);}
+                        ArithmeticTarget::E => {self.registers.e = self.inc_8(self.registers.e);}
+                        ArithmeticTarget::H => {self.registers.h = self.inc_8(self.registers.h);}
+                        ArithmeticTarget::L => {self.registers.l = self.inc_8(self.registers.l);}
+                    }
+                }
+                Instructions::INC16(target) =>
+                {
+                    match target
+                    {
+                        ArithmeticTarget16::AF => {let af = self.registers.get_af(); let result = self.inc_16(af); self.registers.set_hl(result);}
+                        ArithmeticTarget16::BC => {let bc = self.registers.get_bc(); let result = self.inc_16(bc); self.registers.set_hl(result);}
+                        ArithmeticTarget16::DE => {let de = self.registers.get_de(); let result = self.inc_16(de); self.registers.set_hl(result);}
+                        ArithmeticTarget16::HL => {let hl = self.registers.get_hl(); let result = self.inc_16(hl); self.registers.set_hl(result);}
+                    }
+                }
+                Instructions::DEC8(target) =>
+                {
+                    match target
+                    {
+                        ArithmeticTarget::A => {self.registers.a = self.dec_8(self.registers.a);}
+                        ArithmeticTarget::B => {self.registers.b = self.dec_8(self.registers.b);}
+                        ArithmeticTarget::C => {self.registers.c = self.dec_8(self.registers.c);}
+                        ArithmeticTarget::D => {self.registers.d = self.dec_8(self.registers.d);}
+                        ArithmeticTarget::E => {self.registers.e = self.dec_8(self.registers.e);}
+                        ArithmeticTarget::H => {self.registers.h = self.dec_8(self.registers.h);}
+                        ArithmeticTarget::L => {self.registers.l = self.dec_8(self.registers.l);}
+                    }
+                }
+                Instructions::DEC16(target) =>
+                {
+                    match target
+                    {
+                        ArithmeticTarget16::AF => {let af = self.registers.get_af(); let result = self.dec_16(af); self.registers.set_hl(result);}
+                        ArithmeticTarget16::BC => {let bc = self.registers.get_bc(); let result = self.dec_16(bc); self.registers.set_hl(result);}
+                        ArithmeticTarget16::DE => {let de = self.registers.get_de(); let result = self.dec_16(de); self.registers.set_hl(result);}
+                        ArithmeticTarget16::HL => {let hl = self.registers.get_hl(); let result = self.dec_16(hl); self.registers.set_hl(result);}
+                    }
+                }
+                Instructions::CCF() =>
+                {
+                    self.ccf();
+                }
+                Instructions::SCF() =>
+                {
+                    self.scf();
+                }
+                Instructions::RRA() =>
+                {
+                    self.rra();
+                }
             }
         }
     fn add(&mut self, value: u8) -> u8
@@ -282,10 +347,10 @@ impl CPU
         }
     fn addhl(&mut self, value: u16) -> u16
         {
-            let (new_value, overflow) = (self.registers.a as u16).overflowing_add(value);
+            let (new_value, overflow) = self.registers.get_hl().overflowing_add(value);
             self.registers.f.zero = new_value == 0; 
             self.registers.f.subtract = false; 
-            self.registers.f.half_carry = ((self.registers.a as u16) & 0x0FFF) + (value & 0x0FFF) > 0x0FFF; 
+            self.registers.f.half_carry = (self.registers.get_hl() & 0x0FFF) + (value & 0x0FFF) > 0x0FFF; 
             self.registers.f.carry = overflow;
             new_value
         }
@@ -350,4 +415,55 @@ impl CPU
             self.registers.f.half_carry = (self.registers.a & 0x0F) < (value & 0x0F); 
             self.registers.f.carry = overflow;
         }
+    fn inc_8(&mut self, value: u8) -> u8
+    {
+        let (new_value, overflow) = value.overflowing_add(1);
+        self.registers.f.zero = new_value == 0; 
+        self.registers.f.subtract = false; 
+        self.registers.f.half_carry = (value & 0x0F) == 0x0F; 
+        self.registers.f.carry = overflow;
+        new_value
+    }
+    fn inc_16(&mut self, value: u16) -> u16
+    {
+        let (new_value, overflow) = value.overflowing_add(1);
+        self.registers.f.zero = new_value == 0; 
+        self.registers.f.subtract = false; 
+        self.registers.f.half_carry = (value & 0x0FFF) == 0x0FFF; 
+        self.registers.f.carry = overflow;
+        new_value
+    }
+    fn dec_8(&mut self, value: u8) -> u8
+    {
+        let (new_value, overflow) = value.overflowing_sub(1);
+        self.registers.f.zero = new_value == 0; 
+        self.registers.f.subtract = false; 
+        self.registers.f.half_carry = (value & 0x0F) < 1; 
+        self.registers.f.carry = overflow;
+        new_value
+    }
+    fn dec_16(&mut self, value: u16) -> u16
+    {
+        let (new_value, overflow) = value.overflowing_sub(1);
+        self.registers.f.zero = new_value == 0; 
+        self.registers.f.subtract = false; 
+        self.registers.f.half_carry = 1 > (value & 0x0FFF); 
+        self.registers.f.carry = overflow;
+        new_value
+    }
+    fn ccf(&mut self)
+    {
+        self.registers.f.carry = !self.registers.f.carry;
+    }
+    fn scf(&mut self)
+    {
+        self.registers.f.carry = true;
+    }
+    fn rra(&mut self)
+    {
+        let oldcarry = (self.registers.f.carry as u8) << 7;
+        let newcarry = (self.registers.a & 0x01) != 0;
+        self.registers.a = (self.registers.a >> 1) | oldcarry;
+        self.registers.f.carry = newcarry;
+    }
 }
