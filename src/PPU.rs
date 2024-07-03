6ff4c0142c28bae8579876bc62ee7b15fd251c54
Stuart Rossiter <c00284845@setu.ie>
2024-07-03 17:35:03 +0100

    moved OAM to ppu

--- src/PPU.rs
+++ src/PPU.rs
@@ -1,3 +1,10 @@
+pub const VRAM_SIZE: usize = 0x1800;
+pub const TILE_COUNT: usize = 384;
+pub const SCREEN_WIDTH: usize = 160;
+pub const SCREEN_HEIGHT: usize = 144;
+pub const NUMBER_OF_OBJECTS: usize = 40;
+pub const OBJECT_ATTRIBUTE_MEMORY_SIZE: usize = 0xA0;
+
 pub enum Colour
 {
     White = 255,
@@ -34,7 +41,7 @@ impl Palette
         )
     }
 }
-//Palette can be altered, so this must be implemented. Also why above is just 4 colours as opposed to using Colour again.
+//Palette can be altered, so this must be implemented. Also why above is 4 colours as opposed to using Colour again.
 impl std::convert::From<u8> for Palette
 {
     fn from(value: u8) -> Self
@@ -57,10 +64,17 @@ enum TilePixelValue
     Three
 }
 type Tile = [[TilePixelValue; 8]; 8];
+
+fn empty_tile() -> Tile
+{
+    [[TilePixelValue::Zero; 8]; 8]
+}
+#[derive(Clone, Copy)]
 enum ObjectPalette
 {
     Zero, One
 }
+#[derive(Clone, Copy)]
 struct Object
 {
     x: i16,
@@ -71,24 +85,30 @@ struct Object
     pallette: ObjectPalette,
     priority: bool,
 }
-
-fn empty_tile() -> Tile
+impl Object
 {
-    [[TilePixelValue::Zero; 8]; 8]
+    fn new() -> Object
+    {
+        Object
+        {
+            x: 0,
+            y: 0,
+            tile: 0,
+            x_flip: false,
+            y_flip: false,
+            pallette: ObjectPalette::Zero,
+            priority: false,
+        }
+    }
 }
-pub const VRAM_SIZE: usize = 0x1800;
-pub const TILE_COUNT: usize = 384;
-pub const SCREEN_WIDTH: usize = 160;
-pub const SCREEN_HEIGHT: usize = 144;
-pub const NUMBER_OF_OBJECTS: u8 = 40;
 pub struct PPU
 {
     vram: [u8; VRAM_SIZE],
+    oam: [Object; NUMBER_OF_OBJECTS],
     tiles: [Tile; TILE_COUNT],
     mode: PPUModes,
     obp0: Palette,
     obp1: Palette,
-
 }
 
 pub enum PPUModes
@@ -98,6 +118,7 @@ pub enum PPUModes
     VBlank,
     HBlank
 }
+
 impl PPU
 {
     pub fn new() -> PPU
@@ -105,8 +126,11 @@ impl PPU
         PPU
         {
             vram: [0; VRAM_SIZE],
+            oam: [Object::new(); NUMBER_OF_OBJECTS],
             tiles: [empty_tile(); TILE_COUNT],
             mode: PPUModes::OAMScan,
+            obp0: Palette::new(),
+            obp1: Palette::new(),
         }
     }
     pub fn write_to_vram(&mut self, address: usize, value: u8)
